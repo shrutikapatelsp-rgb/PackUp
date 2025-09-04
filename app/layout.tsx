49f7f7bc12613ba2e41cc3518e50ca7a0604e212
shrutikapatelsp-rgb <shrutikapatelsp@gmail.com>
2025-09-04 20:04:42 +0530

    feat: add Supabase bootstrap to layout for browser console access

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import './globals.css';
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -11,28 +12,33 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        {/* Travelpayouts verification */}
-        <script
-          data-noptimize="1"
-          data-cfasync="false"
-          data-wpfc-render="false"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function () {
-                var script = document.createElement("script");
-                script.async = 1;
-                script.src = 'https://tp-em.com/NDUyNzg2.js?t=452786';
-                document.head.appendChild(script);
-              })();
-            `,
-          }}
-        />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
+
+return (
+  <html lang="en">
+    <head>
+      {/* Travelpayouts verification */}
+      <script
+        data-noptimize="1"
+        data-cfasync="false"
+        data-wpfc-render="false"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function () {
+              var script = document.createElement("script");
+              script.async = 1;
+              script.src = 'https://tp-em.com/NDUyNzg2.js?t=452786';
+              document.head.appendChild(script);
+            })();
+          `,
+        }}
+      />
+    </head>
+    <body>
+      {/* Expose supabase client in browser console */}
+      <ClientSupabaseBootstrap />
+      {children}
+    </body>
+  </html>
+);
 }
 
