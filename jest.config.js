916a86915e5d495826352c1738dfe9661bda8048
shrutikapatelsp-rgb <shrutikapatelsp@gmail.com>
2025-09-27 00:15:09 +0530

    test: configure Jest to transform TSX, add alias mapper and style mocks

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }],
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    // support @/ alias used by Next/shadcn imports
+    '^@/(.*)$': '<rootDir>/$1',
+    // stub styles
+    '\\.(css|less|scss|sass)$': '<rootDir>/tests/__mocks__/styleMock.js',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
