9712840698cb181b7359ac55fe9195ecdaa0cc53
shrutikapatelsp-rgb <shrutikapatelsp@gmail.com>
2025-09-27 00:20:43 +0530

    test: use relative import for ItineraryCard

--- tests/ItineraryCard.test.tsx
+++ tests/ItineraryCard.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import ItineraryCard from '@/components/ItineraryCard';
+import ItineraryCard from '../src/components/ItineraryCard';
 
 describe('ItineraryCard', () => {
   it('renders legacy itinerary shape', () => {
